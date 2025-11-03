e5391cf59ecb9a4e311fdb9d621198aaf87984f9
Miikka <Oscar11212@outlook.com>
2025-11-03 12:00:11 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -20,8 +20,8 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
-	subtitle: "One demo website",
+	title: "Miikka's Archive",
+	subtitle: "Welcome to Miikka's Archive",
 
 	lang: SITE_LANG,
 
